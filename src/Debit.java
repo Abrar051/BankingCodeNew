bbed520a3ef742db6843dbcd9e4272c1b7abb382
Abrar051 <75354845+Abrar051@users.noreply.github.com>
2021-01-12 16:19:56 +0600

    Update Debit.java

--- src/Debit.java
+++ src/Debit.java
@@ -1,8 +1,33 @@
+import java.util.Scanner;
 
 public abstract class Debit extends Account {
-	public void debit ()
+	Scanner input = new Scanner(System.in);
+	public int debit (int balance , int moneyChange , int limit)
 	{
-		
+		if (balance==0)
+		{
+			System.out.println ("Sorry your balance is 0");
+		}
+		else 
+		{   //withdraw 1 deposit 2
+			if (moneyChange==1)
+			{
+				//withdraw 
+				System.out.println ("Enter withdrawal amount : ");
+				int withdrawAmmount = input.nextInt();
+				if (withdrawAmmount>balance | withdrawAmmount>limit)
+				{
+					System.out.println("Sorry your balance is insufficient or your limit crossed ");
+					return 0;
+				}
+				else
+				{
+					balance=balance-withdrawAmmount;
+					System.out.println ("Your present balance is "+balance);
+					//return balance;
+				}
+			}
+		}
+		return balance;
 	}
 }
-
