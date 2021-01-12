a30b69e0028d98d919b8a574b75888b41a272d0f
Abrar051 <75354845+Abrar051@users.noreply.github.com>
2021-01-12 16:19:12 +0600

    Update Credit.java

--- src/Credit.java
+++ src/Credit.java
@@ -1,8 +1,34 @@
+import java.util.Scanner;
 
 public abstract class Credit extends Account {
-	public void credit ()
+	Scanner input = new Scanner(System.in);
+	public int credit (int balance , int moneyChange)
 	{
-		
+		if (balance==0)
+		{
+			System.out.println ("Sorry your balance is 0");
+		}
+		else 
+		{   //withdraw 1 deposit 2
+			if (moneyChange==1)
+			{
+				//withdraw 
+				int limit= balance+(balance/2);
+				System.out.println ("Enter withdrawal amount : ");
+				int withdrawAmmount = input.nextInt();
+				if (withdrawAmmount>balance | withdrawAmmount>limit)
+				{
+					System.out.println("Sorry your balance is insufficient or your limit crossed ");
+					return 0;
+				}
+				else
+				{
+					balance=balance-withdrawAmmount;
+					System.out.println ("Your present balance is "+balance);
+					//return balance;
+				}
+			}
+		}
+		return balance;
 	}
 }
-
